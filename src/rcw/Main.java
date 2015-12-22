a01a858cade8d0a36fccceef8c92d6d1a1922554
yoshiki obata <obatay@c240161.intranet.justsystem.co.jp>
2015-12-22 14:31:36 +0900

    spec:3 実行時の引数にレシピのデータのパスを持たせるように修正

--- src/rcw/Main.java
+++ src/rcw/Main.java
@@ -6,17 +6,26 @@ import rcw.manager.RecipeManager;
 import rcw.model.RecipeBuilder.Recipe;
 
 public class Main {
-	
-	private static final String RECIPE_DATA = "recipe-data.txt";
-	
-	public static void main(String args[]){
-	    RecipeManager manager = new RecipeManager();
-	    List<Recipe> recipes = manager.loadFromFile(RECIPE_DATA);
-	    
-	    for(Recipe recipe : recipes){
-	    	System.out.println(recipe.getTitle());
-	    }
-	
-	}
+
+    /**
+     * 引数に実行時に必要なデータを持たせます
+     *
+     * @param args レシピデータがあるパス
+     */
+    public static void main(String args[]) {
+        if (args.length != 1) {
+            // 引数に各種データ（レシピのパスなど）を持たせる
+            // 想定外のデータ数であれば例外を吐く
+            throw new IllegalArgumentException();
+        }
+        String recipe_data_path = args[0];
+        RecipeManager manager = new RecipeManager();
+        List<Recipe> recipes = manager.loadFromFile(recipe_data_path);
+
+        for (Recipe recipe : recipes) {
+            System.out.println(recipe.getTitle());
+        }
+
+    }
 
 }
